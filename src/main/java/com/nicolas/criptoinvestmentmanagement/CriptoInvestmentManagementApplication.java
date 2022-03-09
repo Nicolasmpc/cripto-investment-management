9b4adc0e7887b8324ff743489a2ab9e336db7781
Nicolas Portela <nmpc1997@gmail.com>
2022-03-09 11:09:05 -0300

    first commit

---
+++ src/main/java/com/nicolas/criptoinvestmentmanagement/CriptoInvestmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.criptoinvestmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoInvestmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoInvestmentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/criptoinvestmentmanagement/CriptoInvestmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.criptoinvestmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoInvestmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
